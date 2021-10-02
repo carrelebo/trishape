937dbe088d5d20f0ddae5accbc5ec1b0c0f1ec33
carrelebo <mryoshizefu@gmail.com>
2021-10-02 20:40:27 +0200

    make program more beautiful

--- main.py
+++ main.py
@@ -16,19 +16,24 @@ def pythagore():
     let2 = str(input("donne moi la lettre du deuxième segment : "))
     let3 = str(input("donne moi la lettre du dernier segment : "))
 
-    seg1 = str(input("donne moi le nom du premier segment"))
-    seg2 = str(input("sonne moi le nom du deucième segment"))
-    seg3 = str(input("donne moi le nom du troisème segment"))
-
     triangle = let1, let2, let3
+
+    print("""
     
-    m_seg1 = int(input("combien mesure le segment " + str(seg1) + " ? : "))
-    m_seg2 = int(input("combien mesure le segment " + str(seg2) + " ? : "))
-    m_seg3 = int(input("combien mesure le segment " + str(seg3) + " ? : "))
+    """)
+#seg1 = str(input("donne moi le nom du premier segment : "))
+#seg2 = str(input("sonne moi le nom du deucième segment : "))
+#seg3 = str(input("donne moi le nom du troisème segment : "))
+    
+#triseg = seg1, seg2, seg3
+    
+    m_seg1 = int(input("combien mesure le segment [" + str(let1) + str(let2) + "] ? : "))
+    m_seg2 = int(input("combien mesure le segment [" + str(let1) + str(let3) + "] ? : "))
+    m_seg3 = int(input("combien mesure le segment [" + str(let2) + str(let3) + "] ? : "))
 
     allm = m_seg1, m_seg2, m_seg3
     add_allm = m_seg1 + m_seg2 + m_seg3
 
-    print(f"le trangle {triangle} à un périmètre de {add_allm}")
+    print("le trangle" + str(let1) + str(let2) + str(let3) + "qui a pour segement [" + str(let1) + str(let2) + "]" + "[" + str(let1) + str(let3) + "]" + "[" + str(let2) + str(let3) + "]" + "à un périmètre de " + str(add_allm) + " :)")
 
 pythagore()
