08fc2854f352ad0df4df205d7161ef44daba535c
carrelebo <mryoshizefu@gmail.com>
2021-10-16 17:52:37 +0200

    done perimetre fonciton

--- main.py
+++ main.py
@@ -2,30 +2,26 @@ import math
 import time
 
 print("""
-Bievenue sur l'application console tripshape,
-
-l'application qui utilise le théorème de pythagore
-
+Bievenue sur l'application console tripshape-flushed,
+l'application qui est faites pour faire des calculs avec des triangles
 (ne prend seulement en compte les variable entière "int")
 
 """)
 
-def pythagore():
+def perimetre():
 
-    let1 = str(input("domme moi la lettre du premier segment : "))
-    let2 = str(input("donne moi la lettre du deuxième segment : "))
-    let3 = str(input("donne moi la lettre du dernier segment : "))
+    unity = str(input("quelle unité de longeur utilisez-vous (cm, m, km ect...) ? : "))
+
+    print("")
+
+    let1 = str(input("domme moi la lettre du premier point du triangle (en majuscule) : "))
+    let2 = str(input("donne moi la lettre du deuxième point du triangle (en majuscule) : "))
+    let3 = str(input("donne moi la lettre du troisième point du triangle (en majuscule) : "))
 
     triangle = let1, let2, let3
 
     print("""
-    
     """)
-#seg1 = str(input("donne moi le nom du premier segment : "))
-#seg2 = str(input("sonne moi le nom du deucième segment : "))
-#seg3 = str(input("donne moi le nom du troisème segment : "))
-    
-#triseg = seg1, seg2, seg3
     
     m_seg1 = int(input("combien mesure le segment [" + str(let1) + str(let2) + "] ? : "))
     m_seg2 = int(input("combien mesure le segment [" + str(let1) + str(let3) + "] ? : "))
@@ -34,6 +30,9 @@ def pythagore():
     allm = m_seg1, m_seg2, m_seg3
     add_allm = m_seg1 + m_seg2 + m_seg3
 
-    print("le trangle" + str(let1) + str(let2) + str(let3) + "qui a pour segement [" + str(let1) + str(let2) + "]" + "[" + str(let1) + str(let3) + "]" + "[" + str(let2) + str(let3) + "]" + "à un périmètre de " + str(add_allm) + " :)")
+    print("")
+    print("le trangle " + str(let1) + str(let2) + str(let3) + " qui a pour segements [" + str(let1) + str(let2) + "]" + "[" + str(let1) + str(let3) + "]" + "[" + str(let2) + str(let3) + "] " + "à un périmètre de " + str(add_allm) + str(unity) + " :)")
 
-pythagore()
+    print("")
+
+perimetre()
